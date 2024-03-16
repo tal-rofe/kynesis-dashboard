5f55c7098d25af0103356c8ecd7508348180c6fe
Amir Ben Shimol <amir.benshi@gmail.com>
2024-03-16 17:34:37 +0200

    refactor: 💡 add type module to all package.json files

--- apps/frontend/next.config.cjs
+++ apps/frontend/next.config.cjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
-	webpack: (config) => {
-		config.resolve.alias['canvas'] = false;
-
-		return config;
-	},
 };
 
 module.exports = nextConfig;
