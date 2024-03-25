c96d9f0cd9cfd18e116f71ab58bf96eeafd0da34
kynesis-root <dev@kynesis.io>
2024-03-26 00:24:28 +0200

    refactor: 💡 remove "cursor" handling logic
    remove "cursor" handling logic

--- apps/pixel-enrichment-function/src/utils/vetric-http.ts
+++ apps/pixel-enrichment-function/src/utils/vetric-http.ts
@@ -1,5 +1,4 @@
 import got from 'got';
-import deepmerge from 'deepmerge';
 
 import { API_CALL_RETRIES, API_CALL_TIMEOUT } from '../constants/http-api';
 
@@ -14,7 +13,7 @@ export const vetricHttpGet = async (url: string, linkedinUrl: string, apiKey: st
 
 	const apiUrlWithIdentifier = url.replace(':identifier', linkedinIdentifier);
 
-	let response = await got
+	const response = await got
 		.get(apiUrlWithIdentifier, {
 			headers: { 'x-api-key': apiKey },
 			timeout: { request: API_CALL_TIMEOUT },
@@ -25,36 +24,6 @@ export const vetricHttpGet = async (url: string, linkedinUrl: string, apiKey: st
 			},
 		})
 		.json<VetricResponse>();
-
-	if (response['cursor'] === 'undefined' || response['cursor'] === null) {
-		return response;
-	}
-
-	while (response.cursor !== 'undefined' && response.cursor !== null) {
-		const newApiUrl = new URLSearchParams(apiUrlWithIdentifier);
-
-		newApiUrl.set('cursor', response.cursor!);
-
-		const newResponse = await got
-			.get(newApiUrl.toString(), {
-				headers: { 'x-api-key': apiKey },
-				timeout: { request: API_CALL_TIMEOUT },
-				retry: {
-					limit: API_CALL_RETRIES,
-					methods: ['HEAD', 'OPTIONS', 'TRACE', 'GET'],
-					// * Keep others (backoff limit, ...) as default
-				},
-			})
-			.json<VetricResponse>();
-
-		/**
-		 * * Delete it so the most updated value of "cursor" will be of "newResponse",
-		 * * because maybe "cursor" is missing from "newResponse" - then we might enter infinite loop
-		 */
-		delete response.cursor;
-
-		response = deepmerge(response, newResponse);
-	}
 
 	// * Irrelevant data for customer
 	delete response.cursor;
