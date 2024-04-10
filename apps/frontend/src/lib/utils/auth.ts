dbaf62abd43c2d42f9079624de1a451ba3be70b8
daniel-bar <daniel.b@solvo.cloud>
2024-04-10 17:54:21 +0300

    refactor: 💡 auth

--- apps/frontend/src/lib/utils/auth.ts
+++ apps/frontend/src/lib/utils/auth.ts
@@ -4,8 +4,8 @@ import GoogleProvider from 'next-auth/providers/google';
 export const authOptions: NextAuthOptions = {
 	providers: [
 		GoogleProvider({
-			clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-			clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		}),
 	],
 	secret: process.env.NEXTAUTH_SECRET,
--- apps/frontend/src/middleware.ts
+++ apps/frontend/src/middleware.ts
@@ -1,19 +1,16 @@
 import { getToken } from 'next-auth/jwt';
+import type { JwtPayload } from 'jsonwebtoken';
 import { type NextRequest, NextResponse } from 'next/server';
 
 import { type RoutesPath, routes } from '@/lib/routes';
 import { validateJWT } from './lib/utils/validate-jwt';
 
 const middleware = async (request: NextRequest) => {
-	const { cookies } = request;
 	const { pathname } = request.nextUrl;
 
-	const userTokenCookie = cookies.get('token');
-	const userToken = userTokenCookie ? userTokenCookie.value : null;
+	const token = (await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET })) as JwtPayload;
 
-	const session = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
-
-	const isAuthenticate = Boolean(session) && validateJWT(userToken);
+	const isAuthenticated = token?.exp ? validateJWT(token.exp) : false;
 
 	const authorizedRoutes = Object.values(routes)
 		.filter((route) => route.isRequiredAuth)
@@ -26,11 +23,11 @@ const middleware = async (request: NextRequest) => {
 	const absoluteAuthorizedRoutesURL = new URL(routes.visitors.path, request.nextUrl.origin);
 	const absoluteUnAuthorizedRoutesURL = new URL(routes.login.path, request.nextUrl.origin);
 
-	if (authorizedRoutes.includes(pathname as RoutesPath) && !isAuthenticate) {
+	if (authorizedRoutes.includes(pathname as RoutesPath) && !isAuthenticated) {
 		return NextResponse.redirect(absoluteUnAuthorizedRoutesURL.toString());
 	}
 
-	if (unauthorizedRoutes.includes(pathname as RoutesPath) && isAuthenticate) {
+	if (unauthorizedRoutes.includes(pathname as RoutesPath) && isAuthenticated) {
 		return NextResponse.redirect(absoluteAuthorizedRoutesURL.toString());
 	}
 
