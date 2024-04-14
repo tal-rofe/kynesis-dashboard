e02fc0d9051aaff59e4e76c32f9fa6f12f00e561
kynesis-root <dev@kynesis.io>
2024-04-14 23:33:41 +0300

    refactor: 💡 optional keys for pixel collection data
    optional keys for pixel collection data

--- packages/pixel-enrichment-sqs/src/interfaces/pixel-collection-data.ts
+++ packages/pixel-enrichment-sqs/src/interfaces/pixel-collection-data.ts
@@ -2,7 +2,7 @@ export type PixelCollectionData = {
 	readonly apiIndex: number;
 	readonly originDomain: string;
 	readonly email: string;
-	readonly md5Email: string;
-	readonly firstName: string;
-	readonly lastName: string;
+	readonly md5Email?: string;
+	readonly firstName?: string;
+	readonly lastName?: string;
 };
