dd41a7020f8d0b81f4f7c2929f89a6897152b4c0
Amir Ben Shimol <amir.benshi@gmail.com>
2024-04-28 18:40:33 +0300

    Fix authentication middleware bug

--- apps/frontend-dashboard/src/middleware.ts
+++ apps/frontend-dashboard/src/middleware.ts
@@ -1,13 +1,17 @@
+import { getToken } from 'next-auth/jwt';
+import type { JwtPayload } from 'jsonwebtoken';
 import { type NextRequest, NextResponse } from 'next/server';
 
 import { type RoutesPath, routes } from '@/lib/routes';
 
+import { validateJwt } from './lib/utils/validate-jwt';
 
-const middleware =  (request: NextRequest) => {
+const middleware = async (request: NextRequest) => {
 	const { pathname } = request.nextUrl;
 
+	const token = (await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET })) as JwtPayload;
 
-	const isAuthenticated = true;
+	const isAuthenticated = token?.exp ? validateJwt(token.exp) : false;
 
 	const authorizedRoutes: Partial<RoutesPath>[] = Object.values(routes)
 		.filter((route) => route.isRequiredAuth)
