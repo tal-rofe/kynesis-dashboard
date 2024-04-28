95a24cd3b87e3368d1b37b35e3f5940d79c27f48
Amir Ben Shimol <amir.benshi@gmail.com>
2024-04-28 22:01:52 +0300

    add bash script

---
+++ vercel/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bashecho
+"VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+if [[ "$VERCEL_GIT_COMMIT_REF" == "PRODUCTION"  ]] ; then
+    # Proceed with the build 
+    echo "✅ - Build can proceed"
+    exit 1;
+else  # Don't build 
+    echo "🛑 - Build cancelled"
+    exit 0;
+fi
